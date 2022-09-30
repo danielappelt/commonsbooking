ec20592e0a8307b02b26ac40a13234e8d2299a85
Daniel Appelt <daniel.appelt@gmail.com>
2022-09-30 15:28:29 +0200

    Show always two months on mobile

--- assets/public/js/public.js
+++ assets/public/js/public.js
@@ -966,7 +966,7 @@ document.addEventListener("DOMContentLoaded", () => {
                                 break;
 
                               default:
-                                t.options.numberOfMonths = 1, t.options.numberOfColumns = 1;
+                                t.options.numberOfMonths = 2, t.options.numberOfColumns = 1;
                             }
                             if (t.render(), !t.options.inlineMode) {
                                 var e = t.picker.getBoundingClientRect();
